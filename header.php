9cc245152f9fcdd82a7b31dea3687cc341d074f3
Charles Fiers <charles.fiers@etud.u-picardie.fr>
2017-02-03 14:00:11 +0100

    Ajout du fichier Medias

--- header.php
+++ header.php
@@ -1,16 +1,20 @@
 <header class="blocheader">	
-        <div class="col-5-12">
+        <div class="col-1-12">
         	<h1>Halle au Frais</h1>
         </div>
-        <div class="col-7-12">
+        <div class="col-5-12">
         	<nav>
-        		<ul>
-        		<li><a href="">Accueil</a></li>
-        		<li><a href="">Actualités</a></li>
-        		<li><a href="">Qui sommes-nous ?</></li>
-        		<li><a href="">Evénements</a></li>
-        		<li><a href="">Contact</a></li>
-        		</ul>
+        		<h2>Infos</h2>
         	</nav>
+        </div>
+        <div class="col-5-12">
+                <nav>
+                        <h2>News</h2>
+                </nav>
+        </div>
+        <div class="col-1-12">
+                <nav>
+                        <h2>X</h2>
+                </nav>
         </div>
 </header>
