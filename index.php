ddc0052fb66352df24d9a8aae0c2dd32e516a70d
Charles Fiers <charles.fiers@etud.u-picardie.fr>
2017-02-09 18:44:16 +0100

    Modif index

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-	require 'libs/flight/Flight.php';
-    <title>Halle au Frais</title>
-    <meta charset='utf-8' />
-    <link rel="stylesheet" type="text/css" href="normalize.css" /><!--Reset css-->
-    <link rel="stylesheet" type="text/css" href="simplegrid.css" /><!--Chargement de la grille SimpleGrid-->
-    <link rel="stylesheet" type="text/css" href="style.css" />
-</head>
-<body>
-<div class="grid grid-pad">
-	<?php include("header.php");?>
-	<main>
-		
-	</main>
-	<footer>
-		
-	</footer>
-</div>
-</body>
-</html>
+<?php 
+require 'libs/flight/Flight.php';
+	Flight::route('/', function(){
+    echo 'hello world!';});
+	Flight::start();
