9ec81384a88b7a337454da577fbe684bc6960b35
Adam NAILI <adam.naili@etud.u-picardie.fr>
2017-03-16 21:05:48 +0100

    Début de la refonte de la barre de navigation : Remise en place des balises broken, changement de la barre de nav en liste ul/li

--- views/base.php
+++ views/base.php
@@ -9,58 +9,52 @@
     <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
-<div class="grid grid-pad">
-	<header class="blockheader">
+<!--REFONTE DU HEADER-->
+<header class="blockheader">
+    <div class="grid grid-pad">
+
         <div class="grid grid-pad">
-        <div class="col-1-12">
-        	<img src="Medias/logoHaF.png" id="logo"> <!--changement de logo-->
-        </div>
-        <div class="col-3-12">
-            
-        </div>
-        <div id="bandeau" class="animbando">
-                <div class="col-3-12">
-                        <div class="menu">
-                	<h2 >Infos</h2>
-                        </div>
-                </div>
-                <div class="col-3-12">
-                        <div class="menu">
-                        <h2 >News</h2>
-                </div>
-        </div>
-        </div>
-        <div class="col-1-12">
-            
-        </div>
-        <div class="col-1-12">
+            <div class="col-1-12">
+             <img src="Medias/logoHaF.png" id="logo"> <!--changement de logo-->
+            </div>
+            <div class="col-10-12">
+                <nav id="bandeau" class="animbando">
+                    <ul>
+                        <li>Infos</li>
+                        <li>News</li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-1-12">
                 <img src="Medias/square.png" id="bouton">
+            </div>
         </div>
-</div>
+    </div>
 </header>
+<!--FIN DE REFONTE-->
 
 
-	<main>
-            <?php foreach ($articles as $art) {
-                echo $art->titre;
-                echo $art->contenu;
-            };
-            ?>
-	</main>
-	<footer>
-		
-	</footer>
+<main>
+    <?php foreach ($articles as $art) {
+        echo $art->titre;
+        echo $art->contenu;
+    };
+    ?>
+</main>
+<footer>
+
+</footer>
 </div>
 
 <script>
-var bouton = document.getElementById("bouton");
-var menu1 = document.getElementById("bandeau");
+    var bouton = document.getElementById("bouton");
+    var menu1 = document.getElementById("bandeau");
 
-bouton.addEventListener("click", function() {
-  
-  menu1.classList.toggle("animbando");
-  
-});
+    bouton.addEventListener("click", function() {
+
+      menu1.classList.toggle("animbando");
+
+  });
 </script>
 </body>
 </html>
