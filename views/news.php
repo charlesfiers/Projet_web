f528521d879bd92644e937c4830932d3573d8f84
Charles Fiers <charles.fiers@etud.u-picardie.fr>
2017-03-16 19:00:54 +0100

    Déplacement de trucs au bon endroit

---
+++ views/news.php
@@ -0,0 +1,28 @@
+<?php
+public function callArticles($file)
+{
+	include($file);
+	
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halle au Frais</title>
+    <meta charset='utf-8' />
+    <link rel="stylesheet" type="text/css" href="normalize.css" /><!--Reset css-->
+    <link rel="stylesheet" type="text/css" href="simplegrid.css" /><!--Chargement de la grille SimpleGrid-->
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+	<div class="grid grid-pad">
+	<?php include("header.php");?>
+	<main>
+		
+	</main>
+	</div>
+</body>
+<footer>
+		
+</footer>
+</html>
