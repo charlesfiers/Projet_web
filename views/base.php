29c08331e9c3988881ce64b58ab6f67d318ebc6e
Adam NAILI <adam.naili@etud.u-picardie.fr>
2017-03-17 13:10:33 +0100

    Header déplacé + JS déplacé dans fichier à part.

--- views/base.php
+++ views/base.php
@@ -9,58 +9,17 @@
     <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
-<!--REFONTE DU HEADER-->
-<header id="header_background" class="blockheader">
-    <div class="grid grid-pad">
-
-        <div class="grid grid-pad">
-            <div class="col-1-12">
-             <img src="Medias/logoHaF.png" id="logo"> <!--changement de logo-->
-            </div>
-            <div class="col-10-12">
-                <nav id="bandeau" class="animbando">
-                    <ul>
-                        <li>Infos</li>
-                        <li>News</li>
-                        <li>News</li>
-                        <li>News</li>
-                        <li>News</li>
-                        <li>News</li>
-                    </ul>
-                </nav>
-            </div>
-            <div class="col-1-12">
-                <img src="Medias/square.png" id="bouton">
-            </div>
-        </div>
-    </div>
-</header>
-<!--FIN DE REFONTE-->
+<?php include('./views/header.php');?>
 
 
 <main>
-    <?php foreach ($articles as $art) {
-        echo $art->titre;
-        echo $art->contenu;
-    };
-    ?>
+
 </main>
 <footer>
 
 </footer>
 </div>
 
-<script>
-    var bouton = document.getElementById("bouton");
-    var menu1 = document.getElementById("bandeau");
-    var background = document.getElementById("header_background");
 
-    bouton.addEventListener("click", function() {
-
-      menu1.classList.toggle("animbando");
-      background.classList.toggle("blockheader_color");
-
-  });
-</script>
 </body>
 </html>
--- views/header.php
+++ views/header.php
@@ -1,40 +1,41 @@
-<header class="blockheader">
+<!--REFONTE DU HEADER-->
+<header id="header_background" class="blockheader">
+    <div class="grid grid-pad">
+
         <div class="grid grid-pad">
-        <div class="col-1-12">
-        	<img src="Medias/Logo_HAF.jpg" id="logo">
+            <div class="col-1-12">
+             <img src="Medias/logoHaF.png" id="logo"> <!--changement de logo-->
+            </div>
+            <div class="col-10-12">
+                <nav id="bandeau" class="animbando">
+                    <ul>
+                        <li>Infos</li>
+                        <li>News</li>
+                        <li>News</li>
+                        <li>News</li>
+                        <li>News</li>
+                        <li>News</li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-1-12">
+                <img src="Medias/square.png" id="bouton">
+            </div>
         </div>
-        <div class="col-3-12">
-            
-        </div>
-        <div id="bandeau" class="animbando">
-                <div class="col-3-12">
-                        <div class="menu">
-                	<h2 >Infos</h2>
-                        </div>
-                </div>
-                <div class="col-3-12">
-                        <div class="menu">
-                        <h2 >News</h2>
-                </div>
-        </div>
-        </div>
-        <div class="col-1-12">
-            
-        </div>
-        <div class="col-1-12">
-                <img src="Medias/bouton.jpg" id="bouton">
-        </div>
-</div>
+    </div>
 </header>
+<!--FIN DE REFONTE-->
 
 
 <script>
-var bouton = document.getElementById("bouton");
-var menu1 = document.getElementById("bandeau");
+    var bouton = document.getElementById("bouton");
+    var menu1 = document.getElementById("bandeau");
+    var background = document.getElementById("header_background");
 
-bouton.addEventListener("click", function() {
-  
-  menu1.classList.toggle("animbando");
-  
-});
+    bouton.addEventListener("click", function() {
+
+      menu1.classList.toggle("animbando");
+      background.classList.toggle("blockheader_color"); //Gestion de l'apparition du background de la barre de navigation
+
+  });
 </script>
