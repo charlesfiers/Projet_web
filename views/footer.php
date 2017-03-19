9613c2195d7e367f500e899f8d1d45714beef6b5
Charles Fiers <charles.fiers@etud.u-picardie.fr>
2017-03-19 17:46:53 +0100

    Amélioration bannière

--- views/footer.php
+++ views/footer.php
@@ -1,46 +1,56 @@
 <!-- CODE POUR LA BARRE DE DEFILEMENT-->
 
 <footer>
-	<body onload="plus()">
-<div id="fond"></div>
-<DIV ID="banniere"></DIV>
 
-</body>
+<div id="fond"></div>
+<div id="banniere"></div>
+
+
 
 	<script language="javascript">
-	function plus()
-	{
-		if(chiffre == longueur)
-    	    { chiffre = 0; }
-    	    else
-    	    { chiffre++; }
-	changement = 1;
-	switch(chiffre) {
-    case 0:
-        code_html = '<a href="'+liens[chiffre+2]+'"><img src="'+pictures[chiffre+2]+'" class ="mini"></a><a href="'+liens[chiffre]+'"><img src="'+pictures[chiffre]+'" class ="maxi"></a><a href="'+liens[chiffre+1]+'"><img src="'+pictures[chiffre+1]+'" class ="mini"></a>';
-        break;
-    case 1:
-        code_html = '<a href="'+liens[chiffre-1]+'"><img src="'+pictures[chiffre-1]+'" class ="mini"></a><a href="'+liens[chiffre]+'"><img src="'+pictures[chiffre]+'" class ="maxi"></a><a href="'+liens[chiffre+1]+'"><img src="'+pictures[chiffre+1]+'" class ="mini"></a>';
-        break;
-    case 2:
-        code_html = '<a href="'+liens[chiffre-1]+'"><img src="'+pictures[chiffre-1]+'" class ="mini"></a><a href="'+liens[chiffre]+'"><img src="'+pictures[chiffre]+'" class ="maxi"></a><a href="'+liens[chiffre-2]+'"><img src="'+pictures[chiffre-2]+'" class ="mini"></a>';
-}
-	code_html2 = '<img src="'+pictures[chiffre]+'">';
-	//durée d'affichage d'une bannière en secondes
-	window.setTimeout("plus()", (changement*4000));
-	document.getElementById("fond").innerHTML = code_html2;
-	document.getElementById("banniere").innerHTML = code_html;}
-
 	pictures = new Array()
-	pictures[0] = '<?php echo BASE_URI; ?>Medias/icone1.png';
-	pictures[1] = '<?php echo BASE_URI; ?>Medias/icone2.png';
-	pictures[2] = '<?php echo BASE_URI; ?>Medias/icone3.png';
-	longueur = pictures.length-1;
+		pictures[0] = '<?php echo BASE_URI; ?>Medias/icone1.png';
+		pictures[1] = '<?php echo BASE_URI; ?>Medias/icone2.png';
+		pictures[2] = '<?php echo BASE_URI; ?>Medias/icone3.png';
+		longueur = pictures.length-1;
 
-	liens = new Array()
-	liens[0] = 'lien_image1.html';
-	liens[1] = 'lien_image2.html';
-	liens[2] = 'lien_image3.html';
-	chiffre = 0;
+	var cliq = new objet(0,0);
+
+	plus(cliq);
+
+	function objet(ch, fl){
+		this.chiffre = ch;
+		this.flag = fl;
+	}
+
+	function clique(chiffre, cliq){
+		var cliq2 = new objet(chiffre,0);
+		delete cliq;
+		plus(cliq2);
+	}
+	function plus(objet)
+	{
+		if(objet.chiffre == longueur)
+    	    { objet.chiffre = 0; }
+    	    else
+    	    { objet.chiffre++; }
+		
+		switch(objet.chiffre) {
+    	case 0:
+        	code_html = '<img src="'+pictures[objet.chiffre+2]+'" class ="mini" onclick="clique(1,objet)"><img src="'+pictures[objet.chiffre]+'" class ="maxi" onclick="clique(2,objet)"><img src="'+pictures[objet.chiffre+1]+'" class ="mini" onclick="clique(0,objet)">';
+        	break;
+    	case 1:
+        	code_html = '<img src="'+pictures[objet.chiffre-1]+'" class ="mini" onclick="clique(2,objet)"><img src="'+pictures[objet.chiffre]+'" class ="maxi" onclick="clique(0,objet)"><img src="'+pictures[objet.chiffre+1]+'" class ="mini" onclick="clique(1,objet)">';
+        	break;
+    	case 2:
+        	code_html = '<img src="'+pictures[objet.chiffre-1]+'" class ="mini" onclick="clique(0,objet)"><img src="'+pictures[objet.chiffre]+'" class ="maxi" onclick="clique(1,objet)"><img src="'+pictures[objet.chiffre-2]+'" class ="mini" onclick="clique(2,objet)">';
+		}
+		code_html2 = '<img src="'+pictures[objet.chiffre]+'">';
+
+		document.getElementById("fond").innerHTML = code_html2;
+		document.getElementById("banniere").innerHTML = code_html;
+		window.setTimeout("plus("+objet+")", 4);
+	}
+
 	</script>
 </footer>
